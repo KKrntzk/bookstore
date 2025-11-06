873763a196bc1ed88f4d21ee74a8e693005a31bc
KKrntzk <sophie.kornetzki@gmail.com>
2025-11-06 12:39:10 +0100

    function ideas added

---
+++ script.js
@@ -0,0 +1,7 @@
+// function render all
+// function render title template
+//function render img template
+//function render likes template
+//function price template
+//function comments template
+// function onclick change like amount add class disable greyscale
